e5f42ab2120d23ae92551bff817bad5f9f4a9717
Rafael Melo <rsmelo_@hotmail.com>
2021-06-03 15:47:47 -0300

    start building app

---
+++ lib/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int itemCount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AppBar Demo', style: TextStyle(color: Colors.deepOrange)),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(8),
+        children: <Widget>[
+          Container(
+            height: 50,
+            color: Colors.amber[600],
+            child: const Center(child: Text('Entry A')),
+          ),
+          Container(
+            height: 50,
+            color: Colors.amber[500],
+            child: const Center(child: Text('Entry B')),
+          ),
+          Container(
+            height: 50,
+            color: Colors.amber[100],
+            child: const Center(child: Text('Entry C')),
+          ),
+        ],
+      ),
+    );
+  }
+}
